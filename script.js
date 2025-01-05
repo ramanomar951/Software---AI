54132928cdc4efeff10d7af13133c83325eff628
Raman Software <secondsemistar@gmail.com>
2025-01-05 22:24:09 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 alert(
-  "          بەخێربێن\nبەهیوای سوود وەرگرتن\nئەم ماڵپەرە لەژێرچاککردنە \nدروستکراوە لە لایەن \n          ئەندازیار\n رامان عمر حسن کۆیی"
+  "          بەخێربێن\nبەهیوای سوود وەرگرتن\nئەم ماڵپەرە لەژێرچاککردنە \nدروستکراوە لە لایەن ئەندازیار\n رامان عمر حسن کۆیی"
 );
 const chatBody = document.querySelector(".chat-body");
 const messageInput = document.querySelector(".message-input");
